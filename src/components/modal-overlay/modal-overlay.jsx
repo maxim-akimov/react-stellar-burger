5f8256a6b79e0feed0e16bcaec2dadfec69aef08
maxim-akimov <79629719220@ya.ru>
2023-05-19 06:42:57 +0300

    Оверлей

---
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './modal-overlay.module.css';
+
+
+function ModalOverlay(props) {
+    return (
+        <div className={styles.overlay}>
+            
+        </div>
+    )
+}
+
+
+export default ModalOverlay;
